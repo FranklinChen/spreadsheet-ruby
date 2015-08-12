d3497bfda6ab372ee6d7752009c0b7e863a0ee2f
Franklin Chen <franklinchen@franklinchen.com>
2015-08-12 00:37:56 -0400

    Add more comments, split out adder expression.

--- spec/spreadsheet_spec.rb
+++ spec/spreadsheet_spec.rb
@@ -12,36 +12,63 @@ describe Spreadsheet do
     # exp_of_three_cells = do
     #   a <- (return 1) & cell_exp
     #   b <- (return 2) & cell_exp
-    #   c <- (do
-    #           aValue <- a & exp
-    #           bValue <- b & exp
-    #           return (aValue + bValue)
-    #        ) & cell_exp
+    #   let adder_exp = do
+    #     aValue <- a & exp
+    #     bValue <- b & exp
+    #     return (aValue + bValue)
+    #   c <- adder_exp & cell_exp
     #   return (a, b, c)
-    exp_of_three_cells = Exp.create(1).cell_exp >= ->(a) {
+    #
+    # Within the Exp monad:
+    # - a is a Cell created from Exp initialized with the value 1
+    # - b is a Cell created from Exp initialized with the value 2
+    # - adder_exp is an Exp that when run,
+    #   - binds aValue to an Exp created from Cell a
+    #   - binds bValue to an Exp created from Cell b
+    #   - returns sum of aValue and bValue
+    # - c is a Cell created from adder Exp
+    # - finally, create an Exp collecting the three cells
+    exp_of_three_cells =
+      Exp.create(1).cell_exp >= ->(a) {
       Exp.create(2).cell_exp >= ->(b) {
-        (
-          a.exp >= ->(aValue) {
-            b.exp >= ->(bValue) {
-              Exp.create(aValue + bValue)
-            }
-          }
-        ).cell_exp >= ->(c) {
-          Exp.create([a, b, c])
-        }
-      }
-    }
+      adder_exp =
+        a.exp >= ->(aValue) {
+        b.exp >= ->(bValue) {
+        Exp.create(aValue + bValue)
+        }}
+      adder_exp.cell_exp >= ->(c) {
+      Exp.create([a, b, c])
+    }}}
 
+    # Run the Exp to create three Cells.
     a, b, c = exp_of_three_cells.run
 
+    # c = a + b, when run,
+    #   => 1 + 2
+    #   => 3
     expect(c.exp.run).to eq(3)
 
+    # Set a = 100
+    # So c = a + b, when run,
+    #      => 100 + 2
+    #      => 102
     a.exp = Exp.create(100)
     expect(c.exp.run).to eq(102)
 
-    a.exp = b.exp >= ->(bValue) {
+    # This is the cool part.
+    #
+    # Set a = b * b (an unevaluated expression)
+    # Set b = 4
+    # So c = a + b, when run,
+    #      => (b * b) + b
+    #      => (4 * 4) + 4
+    #      => 16 + 4
+    #      => 20
+    b_doubler_exp =
+      b.exp >= ->(bValue) {
       Exp.create(bValue * bValue)
     }
+    a.exp = b_doubler_exp
     b.exp = Exp.create(4)
 
     expect(c.exp.run).to eq(20)
